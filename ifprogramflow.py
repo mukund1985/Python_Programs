94f2f3e5e3d27018ede416adee61d7e98679d906
mukund1985 <mukund.pandey@gmail.com>
2019-08-24 21:57:58 +0530

    Python Examples

--- ifprogramflow.py
+++ ifprogramflow.py
@@ -8,23 +8,50 @@
 # else:
 #     print(" Please come back in {} Years".format(18 - age))
 
+# print(" Please guess a number between 1 and 10: ")
+# guess = int(input())
+# if guess < 5:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well Done, You guessed it")
+#     else:
+#         print("Please try next time")
+# elif guess > 5:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well Done, You guessed it")
+#     else:
+#         print("Please try next time")
+# else:
+#     print(" Wow, You Guessed it")
+
+# There can be better way to do this logic -
+
 print(" Please guess a number between 1 and 10: ")
 guess = int(input())
-
-if guess <5:
-    print("Please guess higher")
+if guess != 5:
+    if guess < 5:
+        print("Please guess higher")
+    else:  # Guess must be greater than 5
+        print("Please guess lower")
     guess = int(input())
     if guess == 5:
         print("Well Done, You guessed it")
     else:
         print("Please try next time")
-elif guess >5:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == 5:
-        print("Well Done, You guessed it")
-    else:
-        print("Please try next time")
-
 else:
-    print(" Wow, You Guessed it")
+    print("Wow, You guessed it")
+
+####################################
+
+tree1 = "put your first tree name here"
+tree2 = "put your second tree name here"
+
+if tree1 == tree2:
+    print("The trees are the same")
+else:
+    print("The trees are different")
+
+####################################
