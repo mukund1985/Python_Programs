a3d553cd700dd5f3316ae23330fd88042460a81b
mukund1985 <mukund.pandey@gmail.com>
2019-08-26 00:01:04 +0530

    Python Examples

--- ContinueBreakElse.py
+++ ContinueBreakElse.py
@@ -1,3 +1,6 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 for item in shopping_list:
+    if item == "spam":
+        continue             # Continue Stop Processing any more lines in the block and forces the for loop to move on
+# to the next value in the sequence.
     print("Buy " + item)
